4d12bbc02b68ffa6ef0a16f6639205682dd54c21
gouravagarwal <gourav.ag.gourav@gmail.com>
2018-09-15 19:12:35 +0530

    Added connection to db

--- NodeCrud/app.js
+++ NodeCrud/app.js
@@ -2,6 +2,7 @@
 //import dependencies
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 
 //initialise express app
@@ -16,6 +17,19 @@ app.use(express.static(__dirname + '/public'));
 //set ejs as our templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+
+//connect to the database
+//mongoose.connect('mongodb://gourav:Passw0rd@ds141870.mlab.com:41870/asiangames', { useNewUrlParser: true });
+
+mongoose.connect('mongodb://@ds141870.mlab.com:41870/asiangames',{
+    auth:{
+        user:'gourav',
+        password: 'Passw0rd'
+    },
+    useNewUrlParser: true
+})
+
 
 //set routes
 app.use(require('./routes/route'));
